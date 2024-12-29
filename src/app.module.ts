01ce6c7b823dc7d0bda5c15e244968ec65376411
Jimmy Fish <killcoder212@gmail.com>
2024-12-29 18:08:12 +0700

    Scaffolding scheduler and command

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductMapperCheckerCommand } from '@command/product-mapper-checker.command';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductMapperCheckerCommand],
 })
 export class AppModule {}
